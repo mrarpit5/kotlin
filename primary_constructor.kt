9fd7b36804f9a695de0f01f622dfcc82e1ac901d
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:14:23 +0530

    primary constructor

---
+++ primary_constructor.kt
@@ -0,0 +1,12 @@
+class myClass{  
+  
+    constructor(name: String, id: Int){  
+        println("primary constructor")
+println(name)  
+println(id)  
+    }  
+}  
+fun main(args: Array<String>){  
+val myclass = myClass ("arpit", 20)  
+  
+}  
