272393107a80c388909d3cfb795bba35a43ecfe7
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:08:35 +0530

    class demo

---
+++ class2.kt
@@ -0,0 +1,11 @@
+class Demo2 {
+    
+    var empname :String = "arpit"
+    var empage : Int = 20
+    
+}
+fun main(args :Array<String>){
+    var key = Demo2()
+    println(key.empage)
+    println(key.empname)
+}
