9549aec24f3e7e0007b2f5b12ce15807638d5ae7
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:05:57 +0530

    Add files via upload

---
+++ variable.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+   val a = "data"
+    println(a)
+    var b = "data2"
+    println(b)
+    b = "abcd"
+    println(b)
+    
+    var name : String?=null
+    name = "android"
+    println(name)
+}
