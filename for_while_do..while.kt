a4b25dc8b03a2510bbef475c074ba21a4c43b793
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:21:32 +0530

    Add files via upload

---
+++ for_while_do..while.kt
@@ -0,0 +1,45 @@
+fun main(args : Array<String>){
+    
+for(i in 1..5)
+    {print(i)}
+    
+    val number = arrayOf(5,4,3,2,1)
+    
+    println()
+for(item in number)
+    {
+        print(item)
+    }
+
+    var a = 1
+    println()
+    while(a<=5)
+    {
+        print(a)
+        a++
+    }
+    var a2 = 10
+    println()
+    while(a2>=5){
+        print(a2)
+    a2--
+    }
+    
+    
+    println()
+        var i = 1  
+    do {  
+        print(i)  
+        i++  
+    }  
+    while (i<=5);  
+
+    
+    
+    println()
+    
+
+    
+    
+    
+}
