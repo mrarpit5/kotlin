129ac3ab5a7c678613d1a8f95e023eeee1473880
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:09:35 +0530

    class demo

---
+++ class3.kt
@@ -0,0 +1,14 @@
+class Data{
+    
+    var name : String ="arpit"
+    var sub : String = "android"
+    fun display(){
+        println(name)
+        println(sub)
+    }
+}
+fun main(args: Array<String>)
+{
+    var obj = Data()
+    obj.display()
+}
