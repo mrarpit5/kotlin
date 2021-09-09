41dbda4f8f11fc401f6ca157fdf3e888216b4b8a
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:07:30 +0530

    simple class

---
+++ class.kt
@@ -0,0 +1,13 @@
+class Data {  
+    val notice:String ="it will never changed"
+    var age: Int = 20  
+    var name: String =  "arpit"  
+ 
+  
+}  
+fun main(args: Array<String>){  
+    var obj= Data()  
+    println(obj.age)
+    println(obj.notice)
+ 
+}
