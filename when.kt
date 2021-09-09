e16f467a915301bd2bdc5e70b8c1acd52f502a28
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2021-09-09 18:20:15 +0530

    when statement

---
+++ when.kt
@@ -0,0 +1,12 @@
+fun main(args : Array<String>){
+    
+var no = 1
+    when(no){
+        
+        1 -> println("one")
+        2 -> println("two")
+        
+    
+    else -> println("other no")
+    }
+}
